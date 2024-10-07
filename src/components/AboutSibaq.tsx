a119d27b573b4c25e829b1f278576ccdf8c1c065
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-10-08 00:49:24 +0530

    Fix: Changed colourful gradient to b lightb b gradient in AboutSibaq Component

--- src/components/AboutSibaq.tsx
+++ src/components/AboutSibaq.tsx
@@ -15,7 +15,7 @@ const AboutSibaq = () => {
       <div className="text-center my-16 flex justify-center flex-col items-center gap-10">
         <Image src={SibaqLogo} alt="Sibaq Logo" className='scale-85' />
         <p className="text-gray-600 text-3xl tracking-tighter px-32">
-        <span className="text-reveal text-[rgba(0,0,0,0.1)] bg-clip-text bg-gradient-to-r from-y via-r to-b">
+        <span className="text-reveal text-[rgba(0,0,0,0.1)] bg-clip-text bg-gradient-to-r from-black via-[#000000b5] to-black">
         SIBĀQ 2025, Darul Huda National Arts Fest is an intercollegiate arts fest conducted biennially, among under graduate colleges and off campuses of Darul Huda Islamic University. The event is officially sanctioned and supported by Darul Huda Islamic University and its Office of Academic Affairs (OAA) to support and promote development of educational activities of concerned students.
         </span>
         </p>
