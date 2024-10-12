03c7d2d7f997341c69591745302f46f0b31923d5
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-10-12 10:07:21 +0530

    Fix: Notication section Responsive

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -10,43 +10,43 @@ export default function Notifications() {
     }, [])
 
     return (
-        <div className="flex justify-center gap-4 items-center m-10 flex-wrap">
-            <div data-aos="fade-left" className="bg-b text-center transition-all duration-300 hover:bg-opacity-20 hover:scale-[102%] bg-opacity-10 p-10 gap-4 rounded-3xl md:rounded-[70px] flex md:flex-col items-center justify-center w-full md:w-[19rem] md:min-h-[21rem]">
+        <div className="flex justify-center gap-2 md:gap-4 items-center m-10 flex-wrap">
+            <div data-aos="fade-left" className="bg-b md:text-center transition-all duration-300 hover:bg-opacity-20 hover:scale-[102%] bg-opacity-10 px-8 py-6 gap-4 rounded-[1.8rem] md:rounded-[70px] flex md:flex-col items-center justify-center w-full md:w-[19rem] md:min-h-[21rem]">
                 <Image src= {BellIcon} alt="Bell Icon" className="scale-75 md:scale-90" />
-                <div className="flex flex-col items-center gap-4">
-                    <p className="md:text-3xl text-xl leading-6 md:leading-7 tracking-tighter font-medium">Results are out now for controllers</p>
-                    <div className="flex flex-col items-center justify-center gap-2">
-                        <p className="bg-b bg-opacity-10 rounded-full inline px-4 py-1 font-light">Aliya Category</p>
+                <div className="flex flex-col md:items-center gap-2 md:gap-4">
+                    <p className="md:text-3xl text-[1.35rem] leading-6 md:leading-7 tracking-tighter font-medium ml-1 md:ml-0">Results are out now for controllers</p>
+                    <div className="flex md:flex-col items-center md:justify-center gap-2">
+                        <p className="bg-b bg-opacity-10 rounded-full inline px-3 md:px-4 py-0.5 md:py-1 font-light">Aliya Category</p>
                         <p className="opacity-40 text-xs">Today 11:15PM</p>
                     </div>
                 </div>
             </div>
-            <div data-aos="fade-left" className="bg-b text-center transition-all duration-300 hover:bg-opacity-20 hover:scale-[102%] bg-opacity-10 p-10 gap-4 rounded-3xl md:rounded-[70px] flex md:flex-col items-center justify-center w-full md:w-[19rem] md:min-h-[21rem]">
+            <div data-aos="fade-left" className="bg-b md:text-center transition-all duration-300 hover:bg-opacity-20 hover:scale-[102%] bg-opacity-10 px-8 py-6 gap-4 rounded-[1.8rem] md:rounded-[70px] flex md:flex-col items-center justify-center w-full md:w-[19rem] md:min-h-[21rem]">
                 <Image src= {BellIcon} alt="Bell Icon" className="scale-75 md:scale-90" />
-                <div className="flex flex-col items-center gap-4">
-                    <p className="md:text-3xl text-xl leading-6 md:leading-7 tracking-tighter font-medium">Results are out now for controllers</p>
-                    <div className="flex flex-col items-center justify-center gap-2">
-                        <p className="bg-b bg-opacity-10 rounded-full inline px-4 py-1 font-light">Aliya Category</p>
+                <div className="flex flex-col md:items-center gap-2 md:gap-4">
+                    <p className="md:text-3xl text-[1.35rem] leading-6 md:leading-7 tracking-tighter font-medium ml-1 md:ml-0">Results are out now for i now controllers</p>
+                    <div className="flex md:flex-col items-center md:justify-center gap-2">
+                        <p className="bg-b bg-opacity-10 rounded-full inline px-3 md:px-4 py-0.5 md:py-1 font-light">Aliya Category</p>
                         <p className="opacity-40 text-xs">Today 11:15PM</p>
                     </div>
                 </div>
             </div>
-            <div data-aos="fade-left" className="bg-b text-center transition-all duration-300 hover:bg-opacity-20 hover:scale-[102%] bg-opacity-10 p-10 gap-4 rounded-3xl md:rounded-[70px] flex md:flex-col items-center justify-center w-full md:w-[19rem] md:min-h-[21rem]">
+            <div data-aos="fade-left" className="bg-b md:text-center transition-all duration-300 hover:bg-opacity-20 hover:scale-[102%] bg-opacity-10 px-8 py-6 gap-4 rounded-[1.8rem] md:rounded-[70px] flex md:flex-col items-center justify-center w-full md:w-[19rem] md:min-h-[21rem]">
                 <Image src= {BellIcon} alt="Bell Icon" className="scale-75 md:scale-90" />
-                <div className="flex flex-col items-center gap-4">
-                    <p className="md:text-3xl text-xl leading-6 md:leading-7 tracking-tighter font-medium">Results are out now for controllers</p>
-                    <div className="flex flex-col items-center justify-center gap-2">
-                        <p className="bg-b bg-opacity-10 rounded-full inline px-4 py-1 font-light">Aliya Category</p>
+                <div className="flex flex-col md:items-center gap-2 md:gap-4">
+                    <p className="md:text-3xl text-[1.35rem] leading-6 md:leading-7 tracking-tighter font-medium ml-1 md:ml-0">Results are out now for controllers</p>
+                    <div className="flex md:flex-col items-center md:justify-center gap-2">
+                        <p className="bg-b bg-opacity-10 rounded-full inline px-3 md:px-4 py-0.5 md:py-1 font-light">Aliya Category</p>
                         <p className="opacity-40 text-xs">Today 11:15PM</p>
                     </div>
                 </div>
             </div>
-            <div data-aos="fade-left" className="bg-b text-center transition-all duration-300 hover:bg-opacity-20 hover:scale-[102%] bg-opacity-10 p-10 gap-4 rounded-3xl md:rounded-[70px] flex md:flex-col items-center justify-center w-full md:w-[19rem] md:min-h-[21rem]">
+            <div data-aos="fade-left" className="bg-b md:text-center transition-all duration-300 hover:bg-opacity-20 hover:scale-[102%] bg-opacity-10 px-8 py-6 gap-4 rounded-[1.8rem] md:rounded-[70px] flex md:flex-col items-center justify-center w-full md:w-[19rem] md:min-h-[21rem]">
                 <Image src= {BellIcon} alt="Bell Icon" className="scale-75 md:scale-90" />
-                <div className="flex flex-col items-center gap-4">
-                    <p className="md:text-3xl text-xl leading-6 md:leading-7 tracking-tighter font-medium">Results are out now for controllers</p>
-                    <div className="flex flex-col items-center justify-center gap-2">
-                        <p className="bg-b bg-opacity-10 rounded-full inline px-4 py-1 font-light">Aliya Category</p>
+                <div className="flex flex-col md:items-center gap-2 md:gap-4">
+                    <p className="md:text-3xl text-[1.35rem] leading-6 md:leading-7 tracking-tighter font-medium ml-1 md:ml-0">Results are out now for controllers</p>
+                    <div className="flex md:flex-col items-center md:justify-center gap-2">
+                        <p className="bg-b bg-opacity-10 rounded-full inline px-3 md:px-4 py-0.5 md:py-1 font-light">Aliya Category</p>
                         <p className="opacity-40 text-xs">Today 11:15PM</p>
                     </div>
                 </div>
