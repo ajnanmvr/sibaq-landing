99d840ed95b384a8f6ebc8a3fca432d359fbbcc2
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-10-29 22:59:49 +0530

    new responsive sibaq site design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,29 +5,29 @@ import { socialMediaLinks } from "@/libs/socialMediaLinks";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center px-20 py-8">
+    <header className="hidden md:flex justify-between items-center px-20 py-8">
       <Image src={Logo} alt="Logo Sibaq" className="h-14 w-min" />
 
       <nav className="text-[1.4rem] gap-4 flex justify-center items-center font-light">
         <Link href="#" className="text-red">
           Home
         </Link>
-        <Link className="opacity-60" href="#">Results</Link>
-        <Link className="opacity-60" href="#">Events</Link>
-        <Link className="opacity-60" href="#">Contact</Link>
+        <Link className="opacity-60" href="#">
+          Results
+        </Link>
+        <Link className="opacity-60" href="#">
+          Events
+        </Link>
+        <Link className="opacity-60" href="#">
+          Contact
+        </Link>
       </nav>
 
       <div className="flex gap-4">
         {socialMediaLinks.map(({ platform, url, color, icon }, key) => (
           <Link href={url} key={key}>
-            <div
-              className="grayscale hover:grayscale-0"
-            >
-              <Image
-                src={icon}
-                alt={platform + " Icon"}
-                className="w-6 h-6"
-              />
+            <div className="grayscale hover:grayscale-0">
+              <Image src={icon} alt={platform + " Icon"} className="w-6 h-6" />
             </div>
           </Link>
         ))}
