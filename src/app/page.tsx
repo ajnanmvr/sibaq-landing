b7c36ae3799de1b244b9393eddcff76a8a16364c
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-11-06 12:09:33 +0530

    fix: unwanted import remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import CountDown from "@/components/CountDown";
 import CounterCards from "@/components/CounterCards";
 import DHDetails from "@/components/DHDetails";
-import FooterAddress from "@/components/FooterAddress";
 import FooterContact from "@/components/FooterContact";
 import Notifications from "@/components/Notifications";
-// import TimeLine from "@/components/TimeLine";
-// import LiveSection from "@/components/LiveSection";
 import AboutSibaq from "@/components/AboutSibaq";
 import HomeArea from "@/components/HomeArea";
+// import TimeLine from "@/components/TimeLine";
+// import LiveSection from "@/components/LiveSection";
 // import EventTile from "@/components/EventTile";
 
 export default function Home() {
