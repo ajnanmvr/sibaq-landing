5fe1729dc4c02779b6cda710f13eb6b98a62c493
Haris740 <harisharis58651@gmail.com>
2024-11-16 13:09:19 +0530

    fix: downloads

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -1,4 +1,60 @@
+import Link from "next/link";
+
 export default function Downloads() {
+  const data = {
+    "general": [
+      {
+        "title": "Concept Note",
+        "date": "",
+        "path": "/downloads/general-concept-note.pdf",
+      },
+      {
+        "title": "By Law",
+        "date": "",
+        "path": "/downloads/general-bylaw.pdf"
+      },
+      {
+        "title": "Program List",
+        "date": "",
+        "path": "/downloads/general-program-list.pdf"
+      },
+    ],
+    "niics": [
+      {
+        "title": "Concept Note",
+        "date": "",
+        "path": "/downloads/niics-concept-note.pdf",
+      },
+      {
+        "title": "By Law",
+        "date": "",
+        "path": "/downloads/niics-bylaw.pdf"
+      },
+      {
+        "title": "Program List",
+        "date": "",
+        "path": "/downloads/niics-program-list.pdf"
+      },
+    ],
+    "zahrawiyya": [
+      {
+        "title": "Concept Note",
+        "date": "",
+        "path": "/downloads/zahrawiyya-concept-note.pdf",
+      },
+      {
+        "title": "By Law",
+        "date": "",
+        "path": "/downloads/zahrawiyya-bylaw.pdf"
+      },
+      {
+        "title": "Program List",
+        "date": "",
+        "path": "/downloads/zahrawiyya-program-list.pdf"
+      },
+    ],
+  }
+
   return (
     <>
       <div className="md:px-52 flex flex-col justify-center items-center pt-20">
@@ -9,71 +65,29 @@ export default function Downloads() {
           <hr className="border-2 border-blue rounded-full md:w-full w-2/3" />
 
           <div className="w-full px-5 lg:px-0 py-12 grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-5">
-            <div className="group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-yellow border border-yellow/50 hover:border-yellow bg-opacity-10">
-              <div className="bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-yellow/50 group-hover:from-yellow group-hover:via-yellow/50  to-yellow group-hover:to-yellow  transition-colors duration-300 flex-1 rounded-3xl">
-                PDF
-              </div>
-              <div className="p-1 flex flex-col gap-2">
-                <h3 className="text-2xl leading-6 font-medium text-yellow">
-                  <span className="text-sm">SIBAQ 2024</span>
-                  <br /> Concept Note
-                </h3>
-                <div className="flex items-center gap-1">
-                  <div className="bg-yellow h-2 w-2 rounded-full"></div>
-                  <p className="opacity-60 text-sm">01-01-1001</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-red border border-red/50 hover:border-red bg-opacity-10">
-              <div className="bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-red/50 group-hover:from-red group-hover:via-red/50  to-red group-hover:to-red  transition-colors duration-300 flex-1 rounded-3xl">
-                PDF
-              </div>
-              <div className="p-1 flex flex-col gap-2">
-                <h3 className="text-2xl leading-6 font-medium text-red">
-                  <span className="text-sm">SIBAQ 2024</span>
-                  <br /> Concept Note
-                </h3>
-                <div className="flex items-center gap-1">
-                  <div className="bg-red h-2 w-2 rounded-full"></div>
-                  <p className="opacity-60 text-sm">01-01-1001</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-blue border border-blue/50 hover:border-blue bg-opacity-10">
-              <div className="bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-blue/50 group-hover:from-blue group-hover:via-blue/50  to-blue group-hover:to-blue  transition-colors duration-300 flex-1 rounded-3xl">
-                PDF
-              </div>
-              <div className="p-1 flex flex-col gap-2">
-                <h3 className="text-2xl leading-6 font-medium text-blue">
-                  <span className="text-sm">SIBAQ 2024</span>
-                  <br /> Concept Note
-                </h3>
-                <div className="flex items-center gap-1">
-                  <div className="bg-blue h-2 w-2 rounded-full"></div>
-                  <p className="opacity-60 text-sm">01-01-1001</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-green border border-green/50 hover:border-green bg-opacity-10">
-              <div className="bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-green/50 group-hover:from-green group-hover:via-green/50  to-green group-hover:to-green  transition-colors duration-300 flex-1 rounded-3xl">
-                PDF
-              </div>
-              <div className="p-1 flex flex-col gap-2">
-                <h3 className="text-2xl leading-6 font-medium text-green">
-                  <span className="text-sm">SIBAQ 2024</span>
-                  <br /> Concept Note
-                </h3>
-                <div className="flex items-center gap-1">
-                  <div className="bg-green h-2 w-2 rounded-full"></div>
-                  <p className="opacity-60 text-sm">01-01-1001</p>
-                </div>
-              </div>
-            </div>
+            {data?.general?.map((item: any, index: number) => {
+              const color = index % 4 === 0 ? "yellow" : index % 4 === 1 ? "blue" : index % 4 === 2 ? "green" : "red";
+              return (
+                <Link href={item?.path} className={`group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-${color} border border-${color}/50 hover:border-${color} bg-opacity-10`}>
+                  <div className={`bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-${color}/50 group-hover:from-${color} group-hover:via-${color}/50  to-${color} group-hover:to-${color}  transition-colors duration-300 flex-1 rounded-3xl`}>
+                    PDF
+                  </div>
+                  <div className="p-1 flex flex-col gap-2">
+                    <h3 className={`text-2xl leading-6 font-medium text-${color}`}>
+                      <span className="text-sm">SIBAQ 2024</span>
+                      <br /> {item?.title}
+                    </h3>
+                    <div className="flex items-center gap-1">
+                      <div className={`bg-${color} h-2 w-2 rounded-full`}></div>
+                      <p className="opacity-60 text-sm">{item?.date}</p>
+                    </div>
+                  </div>
+                </Link>
+              )
+            })}
           </div>
         </div>
+
         <div className="flex justify-center flex-col items-center">
           <h1 className="font-bold text-center md:text-left text-3xl mb-2">
             NIICS
@@ -81,69 +95,56 @@ export default function Downloads() {
           <hr className="border-2 border-blue rounded-full md:w-full w-2/3" />
 
           <div className="w-full px-5 lg:px-0 py-12 grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-5">
-            <div className="group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-yellow border border-yellow/50 hover:border-yellow bg-opacity-10">
-              <div className="bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-yellow/50 group-hover:from-yellow group-hover:via-yellow/50  to-yellow group-hover:to-yellow  transition-colors duration-300 flex-1 rounded-3xl">
-                PDF
-              </div>
-              <div className="p-1 flex flex-col gap-2">
-                <h3 className="text-2xl leading-6 font-medium text-yellow">
-                  <span className="text-sm">SIBAQ 2024</span>
-                  <br /> Concept Note
-                </h3>
-                <div className="flex items-center gap-1">
-                  <div className="bg-yellow h-2 w-2 rounded-full"></div>
-                  <p className="opacity-60 text-sm">01-01-1001</p>
-                </div>
-              </div>
-            </div>
+            {data?.niics?.map((item: any, index: number) => {
+              const color = index % 4 === 0 ? "yellow" : index % 4 === 1 ? "blue" : index % 4 === 2 ? "green" : "red";
+              return (
+                <Link href={item?.path} className={`group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-${color} border border-${color}/50 hover:border-${color} bg-opacity-10`}>
+                  <div className={`bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-${color}/50 group-hover:from-${color} group-hover:via-${color}/50  to-${color} group-hover:to-${color}  transition-colors duration-300 flex-1 rounded-3xl`}>
+                    PDF
+                  </div>
+                  <div className="p-1 flex flex-col gap-2">
+                    <h3 className={`text-2xl leading-6 font-medium text-${color}`}>
+                      <span className="text-sm">SIBAQ 2024</span>
+                      <br /> {item?.title}
+                    </h3>
+                    <div className="flex items-center gap-1">
+                      <div className={`bg-${color} h-2 w-2 rounded-full`}></div>
+                      <p className="opacity-60 text-sm">{item?.date}</p>
+                    </div>
+                  </div>
+                </Link>
+              )
+            })}
+          </div>
+        </div>
 
-            <div className="group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-red border border-red/50 hover:border-red bg-opacity-10">
-              <div className="bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-red/50 group-hover:from-red group-hover:via-red/50  to-red group-hover:to-red  transition-colors duration-300 flex-1 rounded-3xl">
-                PDF
-              </div>
-              <div className="p-1 flex flex-col gap-2">
-                <h3 className="text-2xl leading-6 font-medium text-red">
-                  <span className="text-sm">SIBAQ 2024</span>
-                  <br /> Concept Note
-                </h3>
-                <div className="flex items-center gap-1">
-                  <div className="bg-red h-2 w-2 rounded-full"></div>
-                  <p className="opacity-60 text-sm">01-01-1001</p>
-                </div>
-              </div>
-            </div>
+        <div className="flex justify-center flex-col items-center">
+          <h1 className="font-bold text-center md:text-left text-3xl mb-2">
+            ZAHRAWIYYA
+          </h1>
+          <hr className="border-2 border-blue rounded-full md:w-full w-2/3" />
 
-            <div className="group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-blue border border-blue/50 hover:border-blue bg-opacity-10">
-              <div className="bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-blue/50 group-hover:from-blue group-hover:via-blue/50  to-blue group-hover:to-blue  transition-colors duration-300 flex-1 rounded-3xl">
-                PDF
-              </div>
-              <div className="p-1 flex flex-col gap-2">
-                <h3 className="text-2xl leading-6 font-medium text-blue">
-                  <span className="text-sm">SIBAQ 2024</span>
-                  <br /> Concept Note
-                </h3>
-                <div className="flex items-center gap-1">
-                  <div className="bg-blue h-2 w-2 rounded-full"></div>
-                  <p className="opacity-60 text-sm">01-01-1001</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-green border border-green/50 hover:border-green bg-opacity-10">
-              <div className="bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-green/50 group-hover:from-green group-hover:via-green/50  to-green group-hover:to-green  transition-colors duration-300 flex-1 rounded-3xl">
-                PDF
-              </div>
-              <div className="p-1 flex flex-col gap-2">
-                <h3 className="text-2xl leading-6 font-medium text-green">
-                  <span className="text-sm">SIBAQ 2024</span>
-                  <br /> Concept Note
-                </h3>
-                <div className="flex items-center gap-1">
-                  <div className="bg-green h-2 w-2 rounded-full"></div>
-                  <p className="opacity-60 text-sm">01-01-1001</p>
-                </div>
-              </div>
-            </div>
+          <div className="w-full px-5 lg:px-0 py-12 grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-5">
+            {data?.zahrawiyya?.map((item: any, index: number) => {
+              const color = index % 4 === 0 ? "yellow" : index % 4 === 1 ? "blue" : index % 4 === 2 ? "green" : "red";
+              return (
+                <Link href={item?.path} className={`group h-64 w-full lg:w-60 p-6 flex flex-col gap-3 relative rounded-3xl bg-${color} border border-${color}/50 hover:border-${color} bg-opacity-10`}>
+                  <div className={`bg-gradient-to-r text-white font-bold items-center justify-center flex text-[2.5rem] from-${color}/50 group-hover:from-${color} group-hover:via-${color}/50  to-${color} group-hover:to-${color}  transition-colors duration-300 flex-1 rounded-3xl`}>
+                    PDF
+                  </div>
+                  <div className="p-1 flex flex-col gap-2">
+                    <h3 className={`text-2xl leading-6 font-medium text-${color}`}>
+                      <span className="text-sm">SIBAQ 2024</span>
+                      <br /> {item?.title}
+                    </h3>
+                    <div className="flex items-center gap-1">
+                      <div className={`bg-${color} h-2 w-2 rounded-full`}></div>
+                      <p className="opacity-60 text-sm">{item?.date}</p>
+                    </div>
+                  </div>
+                </Link>
+              )
+            })}
           </div>
         </div>
       </div>
