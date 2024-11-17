d118b464fa55965227dc003d86b35ea4e74f6c9a
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-11-17 10:23:12 +0530

    update: notification refactor

--- src/components/home/Notifications.tsx
+++ src/components/home/Notifications.tsx
@@ -27,10 +27,9 @@ export default function Notifications() {
           onClick={() => openPopup(notification)}
           className="cursor-pointer bg-blue md:text-center hover:bg-opacity-15 bg-opacity-10 px-4 py-6 gap-4 rounded-[1.8rem] md:rounded-[70px] flex md:flex-col items-center md:justify-center w-full md:w-[19rem] md:min-h-[21rem]"
         >
-          <div className="w-8">
-            {" "}
+          <div className="w-8 md:w-14">
             <Bell className="rounded-full w-8 h-8 aspect-square bg-blue md:w-14 md:h-14" />
-          </div>{" "}
+          </div>
           <div className="flex flex-col md:items-center gap-2 md:gap-4">
             <p className="md:text-3xl text-[1.35rem] leading-6 md:leading-7 tracking-tighter font-medium ml-1 md:ml-0 line-clamp-2">
               {notification.title}
