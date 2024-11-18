70625a588ca8f44f3123501323850426045405e4
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-11-18 16:11:41 +0530

    fix: favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const dm_sans = DM_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "SIBĀQ 2025",
+  title: "SIBĀQ 2025 | Darul Huda National Arts Fest",
   description:
     "SIBĀQ 2025, the Darul Huda National Arts Fest, is a biennial intercollegiate festival celebrating creativity, tradition, and cultural heritage. Featuring skill-oriented contests in diverse fields, it fosters talent, critical thinking, and extracurricular excellence among students, making it an integral part of Darul Huda Islamic University’s commitment to holistic education.",
 };
@@ -25,12 +25,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Sibaq 2025" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${dm_sans.variable} text-black select-none font-sans tracking-tighter`}
       >
-        <Nav />
-        {children}
-        <FooterAddress />
+          <Nav />
+          {children}
+          <FooterAddress />
       </body>
       <GoogleAnalytics gaId="G-ZFS1FXX4BH" />
     </html>
