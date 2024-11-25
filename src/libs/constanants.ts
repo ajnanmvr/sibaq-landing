dd9a8490cd2a7fcd0a495fa7d0990e5bfea097e2
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-11-25 18:54:27 +0530

    update:selection round time changed

--- src/libs/constanants.ts
+++ src/libs/constanants.ts
@@ -1,2 +1,2 @@
-export const COUNTDOWN_DATE = "2024-11-29T00:00:00"; // Nov 29, 2024
+export const COUNTDOWN_DATE = "2024-11-29T16:00:00"; // Nov 29, 2024, 04:00 PM
 export const GRAPHQL_API_URI = "https://api.sibaq.in/graphql";
