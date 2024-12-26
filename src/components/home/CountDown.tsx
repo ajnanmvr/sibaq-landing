5035cc541b3235978916dee632152a3101cbff98
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-12-26 10:23:27 +0530

    update: clean up code formatting and remove unnecessary comments

--- src/components/home/CountDown.tsx
+++ src/components/home/CountDown.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -50,9 +50,16 @@ export default function CountDown() {
   return (
     <div className="md:px-28 flex flex-col md:flex-row justify-between items-center pb-1 md:pb-16 p-5 gap-5 mt-20 md:gap-0 w-full">
       <div className="flex flex-col justify-center items-center gap-1 md:items-start">
-        <p className="text-[2rem] leading-8 text-center md:text-left md:text-[45px] font-medium mb-2 md:mb-6 tracking-tighter">Grand Finale</p>
-        <p className="font-light tracking-widest uppercase md:hidden">starts in</p>
-        <Link href="" className="bg-red rounded-full py-1 pl-1.5 pr-5 text-xl gap-2 text-white items-center hidden md:inline-flex">
+        <p className="text-[2rem] leading-8 text-center md:text-left md:text-[45px] font-medium mb-2 md:mb-6 tracking-tighter">
+          Grand Finale
+        </p>
+        <p className="font-light tracking-widest uppercase md:hidden">
+          starts in
+        </p>
+        <Link
+          href=""
+          className="bg-red rounded-full py-1 pl-1.5 pr-5 text-xl gap-2 text-white items-center hidden md:inline-flex"
+        >
           <Image src={CalenderIcon} className="scale-90" alt="Calendar Icon" />
           Add to Calendar
         </Link>
@@ -81,7 +88,10 @@ export default function CountDown() {
             <p className="text-lg md:text-3xl font-light">Minutes</p>
           </div>
           <div className="text-center">
-            <div suppressHydrationWarning={true} className="bg-red animate-pulse text-3xl md:text-[48px] mb-1 md:mb-2 rounded-full w-[4.5rem] h-[4.5rem] md:w-28 md:h-28 inline-flex items-center justify-center text-white font-bold p-1">
+            <div
+              suppressHydrationWarning={true}
+              className="bg-red animate-pulse text-3xl md:text-[48px] mb-1 md:mb-2 rounded-full w-[4.5rem] h-[4.5rem] md:w-28 md:h-28 inline-flex items-center justify-center text-white font-bold p-1"
+            >
               {timeLeft.seconds}
             </div>
             <p className="text-lg md:text-3xl font-light">Seconds</p>
@@ -89,11 +99,19 @@ export default function CountDown() {
         </div>
       </div>
 
-      <Link href="" className="text-white items-center mt-4 md:hidden inline-flex">
-        <Image src={CalenderIcon} className="border-[5px] z-10 box-content rounded-full border-red" alt="Calendar Icon" />
-        <p className="bg-red rounded-r-full -ml-3 py-1.5 pl-4 pr-4 text-lg">Add to Calendar</p>
+      <Link
+        href=""
+        className="text-white items-center mt-4 md:hidden inline-flex"
+      >
+        <Image
+          src={CalenderIcon}
+          className="border-[5px] z-10 box-content rounded-full border-red"
+          alt="Calendar Icon"
+        />
+        <p className="bg-red rounded-r-full -ml-3 py-1.5 pl-4 pr-4 text-lg">
+          Add to Calendar
+        </p>
       </Link>
-
     </div>
   );
 }
