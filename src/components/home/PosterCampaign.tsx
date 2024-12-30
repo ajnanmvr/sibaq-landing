b68f8fb08ed081d3a3659c1d61b4d33f25e765f3
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-12-30 10:20:07 +0530

    fix: update button text and improve layout in PosterCampaign

--- src/components/home/PosterCampaign.tsx
+++ src/components/home/PosterCampaign.tsx
@@ -107,7 +107,7 @@ const PosterCampaign = () => {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="flex items-center justify-center flex-col sm:flex-row my-3 mx-2 space-y-8 sm:space-y-0 p-10 bg-white shadow-lg rounded-3xl w-96 sm:w-[40rem] ">
+      <div className="flex items-center justify-center flex-col sm:flex-row my-3 mx-2 space-y-8 sm:space-y-0 p-10 bg-white rounded-3xl w-96 sm:w-[40rem] ">
         <div className="overflow-hidden w-[20rem] sm:w-20rem rounded-3xl">
           <div className="container poster bg-white relative ">
             <img src="/campaign-poster.jpg" alt="" className="w-full" />
@@ -129,7 +129,7 @@ const PosterCampaign = () => {
         </div>
         <div className="flex flex-col w-[20rem] justify-start px-4 sm:pr-0 sm:pl-8">
           <h1 className={`${nippo.className} text-2xl text-center leading-7`}>
-            Participating in <br /> Sibaq Poster Campaign
+            Participate in <br /> Sibaq Poster Campaign
           </h1>
           <form onSubmit={handleSubmit} className="flex flex-col mt-2">
             <input
@@ -141,9 +141,11 @@ const PosterCampaign = () => {
             />
             <button
               type="submit"
-              className="bg-red rounded-full py-3 px-4 gap-2 text-white items-center md:inline-flex"
+              className="bg-red flex items-center justify-center rounded-full py-3 text-center px-4 gap-2 text-white"
             >
-              Select Image
+              <svg xmlns="http://www.w3.org/2000/svg" className="fill-white h-4 w-4" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512"><path d="M8.5,5c.83,0,1.5,.67,1.5,1.5s-.67,1.5-1.5,1.5-1.5-.67-1.5-1.5,.67-1.5,1.5-1.5Zm7.32,3.18l-.35-1.42c-.11-.44-.51-.76-.97-.76s-.86,.31-.97,.76l-.35,1.41-1.4,.32c-.45,.1-.77,.5-.77,.96,0,.46,.3,.86,.74,.98l1.43,.39,.36,1.43c.11,.44,.51,.76,.97,.76s.86-.31,.97-.76l.35-1.42,1.42-.35c.44-.11,.76-.51,.76-.97s-.31-.86-.76-.97l-1.42-.35Zm.79-3.3l1.76,.74,.7,1.75c.15,.38,.52,.63,.93,.63s.78-.25,.93-.63l.7-1.74,1.74-.7c.38-.15,.63-.52,.63-.93s-.25-.78-.63-.93l-1.74-.7-.7-1.74c-.15-.38-.52-.63-.93-.63s-.78,.25-.93,.63l-.69,1.73-1.73,.66c-.38,.14-.64,.51-.65,.92,0,.41,.23,.78,.61,.94Zm7.39,4.12v10c0,2.76-2.24,5-5,5H5c-2.76,0-5-2.24-5-5V5C0,2.24,2.24,0,5,0H15c.55,0,1,.45,1,1s-.45,1-1,1H5c-1.65,0-3,1.35-3,3v6.59l.56-.56c1.34-1.34,3.53-1.34,4.88,0l5.58,5.58c.54,.54,1.43,.54,1.97,0l.58-.58c1.34-1.34,3.53-1.34,4.88,0l1.56,1.56V9c0-.55,.45-1,1-1s1,.45,1,1Zm-2.24,11.17l-2.74-2.74c-.56-.56-1.48-.56-2.05,0l-.58,.58c-1.32,1.32-3.48,1.32-4.8,0l-5.58-5.58c-.56-.56-1.48-.56-2.05,0l-1.98,1.98v4.59c0,1.65,1.35,3,3,3h14c1.24,0,2.3-.75,2.76-1.83Z" /></svg>
+
+              Create with your photo
             </button>
             <Modal
               isOpen={isModalOpen}
